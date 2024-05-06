7e79f26e2bd1784854b2f6bdef90f86ab2867749
JoLo5 <167130528+JoLo5@users.noreply.github.com>
2024-05-06 23:24:44 +0200

    Add files via upload

---
+++ R/Notizen_Vorlesung/Vorlesung_23_04_24/Notizen_23_04_24.R
@@ -0,0 +1,55 @@
+# Tipps und Tricks
+
+# Lange Dokumente mit Outline (STRG + SHIFT + O)
+
+# Coding Tipps
+# Code von innen nach außen lesen
+npk[npk$yield<60,]
+npk[,"yield"]
+npk[,"yield"<60]
+
+berryFunctions::dataStr()
+# Verschachtelte Informationen vs Pipe Operator
+max(apply(apply(infert[,-1], 2, range),2, median))
+
+# zuerst Matrix, Parallele in Excel --> Tabelle
+# Umfang
+# 2pir
+erde <- 40000000/(2*pi)
+seil <- 40000001/(2*pi)
+sonne <- 4379000000/(2*pi)
+mond <- 10921000/(2*pi)
+sonneSeil <- 4379000001/(2*pi)
+mondSeil <- 10921001/(2*pi)
+erde-seil
+sonne - sonneSeil
+mond - mondSeil
+browser()
+# 1/2pi Proportionalität egal wie groß Ausgangswert 
+# Fließkommazahlen nie mit == vergleichen (Deziamlkommazahlen), mit Funktion
+# identical, round-function, bereich angegeben z.B. x1-x2 < 0,002
+# cmd shift links --> zeile markieren
+# Haltepunkte sinnvoll, um Code besser nachvollzuziehen
+# Ereignis hat Raum Zeit, jeder Datenpunkt kann ich einem Zeitpunkt und einem Ort Raum zuordnen 
+# Probleme bei Unternehmen: Daten nicht im historischem Verlauf speichern
+# Bytebyte go (Data Pipeline Overview)
+# Development Operations Entwicklung Betrieb von Software
+# Selenium = Test Tool in Python, Selenium: Tracking Codes auslesen von Lieferanten
+# docker container bauen --> Problem: läuft auf allen Geräten? Lösung: Docker --> eine Komplett-Lösung anbieten --> funktion bei Allen gewährleistet
+# ci ce continuous integration continuous 
+# Nagios ähnlich zu Aktivitätsanzeige
+# Simian Army 
+# Devops in 5 min simplylearn
+# Devops Learning Path for Beginners https://www.youtube.com/shorts/DBEL1xjxr4o
+# graphana  Pitzl Microsoft Azure
+# DevOps 
+# Github: erste Aufgabe 1.8!! Abgabe: LINK Alle Übungsaufgaben abspeichern
+
+
+
+data("infert")
+
+# Funktionen
+
+# Verschiedenes
+
